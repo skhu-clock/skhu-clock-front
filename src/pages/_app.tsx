d86724548cff7d92db9ad75d6825c15db57f43a1
zinu <doglife222@gmail.com>
2023-08-01 18:19:04 +0900

    style: 메인 컨텐츠 가운데 정렬

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,25 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 import type { AppProps } from 'next/app';
 import '../styles/index.css';
-import { ThemeProvider,} from '@mui/system';
+import { ThemeProvider } from '@mui/system';
 import theme from '@/styles/theme';
-
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
+      <div css={mainHome}>
+        <Component {...pageProps} />
+      </div>
     </ThemeProvider>
   );
 }
+
+const mainHome = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: auto;
+  height: 100vh;
+`;

d7787897a93c3f6249c1da55af8d2225265e2a20
Kim0426 <706shin1728@naver.com>
2023-08-29 02:14:56 +0900

    fix:api타입 수정

--- src/hooks/useSubWay.ts
+++ src/hooks/useSubWay.ts
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 import type { SubWayAPi } from '@/types/index';
 
 export const useSubwayAPi = () => {
-  const [subway, setSubway] = useState<SubWayAPi | null>(null);
+  const [subway, setSubway] = useState<SubWayAPi[] | null>(null);
 
   useEffect(() => {
     const fetchSubwayData = async () => {

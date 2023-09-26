983fdc360baac1ba160b14dc3cb48af3af77f779
yeeZinu <doglife222@gmail.com>
2023-09-27 01:17:52 +0900

    refactor(#19): 지하철 데이터 최적화

--- src/components/SubWayList/index.tsx
+++ src/components/SubWayList/index.tsx
@@ -1,161 +1,128 @@
 import styled from '@emotion/styled';
 import Image from 'next/image';
-import { useSubwayAPi } from '@/hooks/useSubWay';
-import { useState, CSSProperties } from 'react';
-import Skeleton from '../Common/Skeleton';
-import SubwayTitle from '../Domain/SubwayTitle';
-// import SubwaySeven from '../Domain/SubWaySeven';
-import UplineOne from './UplineOne';
-import UplineSeven from './UplineSeven';
-import UnderLineOne from './UnderLineOne';
-import UnderLineSeven from './UnderlineSeven';
+import { useState, useEffect, MouseEvent } from 'react';
+
+interface SubwayInfo {
+  subwayId: number;
+  updnLine: string;
+  trainLineNm: string;
+  arvlMsg1: string;
+  arvlMsg2: string;
+  arvlCd: number;
+}
+
+interface SubwayData {
+  subway: SubwayInfo[];
+  updnLine: string;
+  subwayId: string;
+}
 
 const SubwayList = () => {
   const [fetchFlag, setFetchFlag] = useState<boolean>(false);
-  const { subway, isLoading } = useSubwayAPi(fetchFlag);
+  const [subwayData, setSubwayData] = useState<SubwayData[]>([]);
 
-  const list: {
-    subwayId: number;
-    updnLine: string;
-    trainLineNm: string;
-    statnNm: string;
-    arvlMsg1: string;
-    arvlMsg2: string;
-    arvlMsg3: string;
-    arvlCd: number;
-  }[] = [];
+  const fetchData = async () => {
+    try {
+      // 데이터를 가져오는 비동기 처리 (예: fetch, axios 등 사용)
+      const response = await fetch('/api/subway');
+      const data = await response.json();
+      await setSubwayData(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
 
-  // (12){}
-  if (!isLoading) {
-    subway?.forEach((item) => list.push(item.subway));
-    console.log(list[1]);
-    
-    const a = subway?.filter((item) => item.subway.subwayId === 1001);
-  }
+  useEffect(() => {
+    // 컴포넌트가 마운트될 때 데이터를 가져옴
+    fetchData();
+  }, [fetchFlag]);
 
-  // 페이지네이션
   // 새로고침
-  const handleClickFetchButton = () => {
+  const handleClickFetchButton = (e: MouseEvent<HTMLImageElement>) => {
+    e.preventDefault();
     setFetchFlag(!fetchFlag);
   };
 
   return (
-    <div style={subwayStyles}>
-      <div style={subwayTitleWrapperStyles}>
-        <div style={subwayTitleListStyles}>
-          <LineNo1>1호선</LineNo1>
-          <div style={subwayLinewayStyles}>
-            <Lineway>역곡 방면</Lineway>
-            <Lineway>오류동 방면</Lineway>
-          </div>
-        </div>
-        <div style={subwayTitleListStyles}>
-          <LineNo7>7호선</LineNo7>
-          <div style={subwayLinewayStyles}>
-            <Lineway>까치울 방면</Lineway>
-            <Lineway>천왕 방면</Lineway>
-          </div>
-        </div>
-      </div>
-      {isLoading && <Skeleton.Box width={450} height={180} />}
-
-      {!isLoading && (
-        <div>
-          <div style={subwayListStyles}>
-            <SubwayTitle
-              renderItem={UplineOne}
-              items={subway?.slice(0, 5) || []}
-            />
-            <SubwayTitle
-              renderItem={UnderLineOne}
-              items={subway?.slice(0, 5) || []}
-            />
-            <SubwayTitle
-              renderItem={UplineSeven}
-              items={subway?.slice(0, 5) || []}
-            />
-            <SubwayTitle
-              renderItem={UnderLineSeven}
-              items={subway?.slice(0, 5) || []}
-            />
-          </div>
-          <ButtonLists>
-            <Image
-              onClick={handleClickFetchButton}
-              src="/reload.png"
+    <div>
+      <ButtonLists onClick={handleClickFetchButton}>
+        <Image
+          src="/reload.png"
+          style={{
+            cursor: 'pointer',
+          }}
+          alt="다시로드하는 이미지"
+          width={30}
+          height={30}
+        />
+      </ButtonLists>
+      <ul
+        style={{
+          display: 'flex',
+          gap: '1rem',
+          margin: '1rem',
+        }}
+      >
+        {subwayData.map((item, index) => (
+          <EverySubwayLine key={index}>
+            <LineColor line={item.subwayId}>{`${item.subwayId}`}</LineColor>
+            <Lineway>{item.updnLine}</Lineway>
+            <ul
               style={{
-                cursor: 'pointer',
+                display: 'flex',
+                flexDirection: 'column',
+                gap: '0.5rem',
               }}
-              alt="다시로드하는 이미지"
-              width={30}
-              height={30}
-            />
-          </ButtonLists>
-        </div>
-      )}
+            >
+              {item.subway.map((subwayInfo, subwayIndex) => (
+                <li key={subwayIndex}>
+                  <p>
+                    {subwayInfo.arvlMsg1.length < 7 ? (
+                      <span>{subwayInfo.arvlMsg1}</span>
+                    ) : (
+                      <span
+                        style={{
+                          wordBreak: 'keep-all',
+                        }}
+                      >
+                        {subwayInfo.arvlMsg2} 도착
+                      </span>
+                    )}
+                  </p>
+                </li>
+              ))}
+            </ul>
+          </EverySubwayLine>
+        ))}
+      </ul>
     </div>
   );
 };
 
 export default SubwayList;
 
-const subwayStyles: CSSProperties = {
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  alignItems: 'center',
-};
-
-const subwayTitleWrapperStyles: CSSProperties = {
-  display: 'flex',
-  flexDirection: 'row',
-  gap: '2rem',
-};
-
-const subwayTitleListStyles: CSSProperties = {
-  display: 'flex',
-  width: '12rem',
-  flexDirection: 'column',
-  textAlign: 'left',
-  marginBottom: '1rem',
-};
-
-const subwayLinewayStyles: CSSProperties = {
-  display: 'flex',
-  flexDirection: 'row',
-  justifyContent: 'space-between',
-  textAlign: 'left',
-};
-
-const subwayListStyles: CSSProperties = {
-  display: 'flex',
-  flexDirection: 'row',
-  gap: '2rem',
-};
+const EverySubwayLine = styled.li`
+  width: 6rem;
+`;
 
 const ButtonLists = styled.div`
   display: flex;
-  justify-content: center;
+  justify-content: flex-end;
   align-items: center;
+  margin-top: 1rem;
+  margin-right: 1rem;
+
   gap: 1rem;
 `;
 
-const LineNo1 = styled.div`
+const LineColor = styled.h2<{ line: string }>`
   font-weight: 700;
-  color: #0052a4;
-  margin-top: 1rem;
-  margin-bottom: 0.5rem;
+  color: ${(props) => (props.line === '1호선' ? '#0052A4' : '#747F00')};
 `;
 
-const LineNo7 = styled.div`
-  font-weight: 700;
-  color: #747f00;
-  margin-top: 1rem;
-  margin-bottom: 0.5rem;
-`;
-
-const Lineway = styled.div`
+const Lineway = styled.p`
   width: 5rem;
   font-weight: 700;
   text-align: left;
+  margin-bottom: 0.5rem;
 `;
